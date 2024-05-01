11cee9547870b13339c70d98898867de17491288
Ruchi agarwal <ruchi.agarwal2005@gmail.com>
2024-05-01 09:44:56 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { auth } from "./config";
+import { signInWithPopup,FacebookAuthProvider } from "firebase/auth";
+
+
 function App() {
+  console.log("App",process.env.REACT_APP_APIKEY);
+  const facebook=()=>{
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log("Facebook Login Successful", user);
+      })
+      .catch((error) => {
+        if (error.code === 'auth/popup-closed-by-user') {
+          console.log("Error: User closed the Facebook login popup before signing in.");
+          // alert("Oops! It looks like you closed the login popup before you could sign in. Please try again, and make sure to allow popup windows if necessary.");
+        } else {
+          console.log("Error during Facebook login", error);
+          // alert("An error occurred during the login process. Please try again later.");
+        }
+      });
+       
+       
+
+   
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={()=>facebook()}>FacebookButton</button>
     </div>
   );
 }
